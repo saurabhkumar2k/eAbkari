72e0974c36a55599e79b497c382cf9e65cd6eab1
Kumar Saurabh <saurabhkumar2k@gmail.com>
2026-05-12 14:32:16 +0530

    Uploaded new project

--- Backend/CoreApi/DTOs/LiquorCategoryDto.cs
+++
@@ -1,8 +0,0 @@
-namespace CoreAPI.DTOs
-{
-    public class LiquorCategoryDto
-    {
-        public string LiquorCatCode { get; set; } = string.Empty;
-        public string Category { get; set; } = string.Empty;
-    }
-}
