82132a9a9f66d26747e274ce7b894b5454dc6474
Muskan Chauahan <mitamaai.25@gmail.com>
2026-06-10 15:29:08 +0530

    Removal of Basic detail page

--- Backend/backend.API/obj/Debug/net9.0/backend.API.AssemblyInfo.cs
+++ Backend/backend.API/obj/Debug/net9.0/backend.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f05d5136b83105936ae289bc9a218c33c2858a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd77e7dc94bcc57548c9e690878d33c8cfc9d521")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
